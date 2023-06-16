67c04a385daf0fd5a4ee048a5411f27ebaf64fc3
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-06-16 16:37:31 +0700

    change queryClient options

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,14 @@ import { router } from './routes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
