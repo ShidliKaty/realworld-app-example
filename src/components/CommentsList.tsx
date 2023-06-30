bce3c53249648fbe9922ca077f7e0b0aeaf3a7c0
Katy <115113751+ShidliKaty@users.noreply.github.com>
2023-06-30 22:48:45 +0700

    add spinner

--- src/components/CommentsList.tsx
+++ src/components/CommentsList.tsx
@@ -1,16 +1,18 @@
 import { useParams } from 'react-router-dom'
 import { useComments } from '../hooks/useComments'
 import { CommentCard } from './CommentCard'
-import { Stack } from '@chakra-ui/react'
+import { Spinner, Stack, Text } from '@chakra-ui/react'
 import { useCommentsStore } from '../store'
 
 export const CommentsList = () => {
   const { slug } = useParams()
-  useComments(slug!)
+  const { data, isLoading } = useComments(slug!)
   const newComment = useCommentsStore((s) => s.newComment)
   const comments = useCommentsStore((s) => s.comments)
   return (
     <Stack alignItems='center' justifyContent='center'>
+      {isLoading && <Spinner />}
+      {(!data || data.comments.length === 0) && <Text>No comments...</Text>}
       {newComment ? <CommentCard comment={newComment.comment} /> : null}
       {comments.map((comment) => (
         <CommentCard key={comment.id} comment={comment} />
